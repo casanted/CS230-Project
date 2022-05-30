9bdf3b9843202f10e0eb944cdc4ed0ecfeeda98f
Carolyn Asante-Dartey <67062673+casanted@users.noreply.github.com>
2022-05-30 01:10:05 -0700

    Obtain first 5 captions for each piece

--- preprocess.py
+++ preprocess.py
@@ -27,7 +27,19 @@ def loadPreprocessedImages():
     print(y_labels)
     y_labels = np.asarray(y_labels)
     np.savetxt("labels.txt", y_labels, delimiter=", ")
-
-
+    
+    
+def getCaptions():
+    # train - 76031, val - 2000, test - 2000
+    df = pd.read_csv("./data/artemis_dataset_release_v0.csv")
+    captions = defaultdict(list)
+    for index, row in df.iterrows():
+        if len(captions[row[1]]) < 5:
+            captions[row[1]].append(row[3])
+    print("Done adding to dict!")
+    with open("captions.txt", 'w') as f:
+        print(captions, file=f)
+        
+        
 if __name__ == "__main__":
     loadPreprocessedImages()
